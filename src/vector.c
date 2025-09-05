26637a413dc31ff6839107a8ad660351056c767a
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-05 12:34:06 +0000

    getter for vector

--- src/vector.c
+++ src/vector.c
@@ -6,5 +6,12 @@ void initVector(float *vectorData, const float *source, size_t size)
     if (vectorData == NULL || source == NULL)
         return;
 
-    memcpy(vectorData, source, size);
+    memcpy(vectorData, source, size * sizeof(float));
+}
+
+float getVector(float *vectorData, size_t size, size_t index)
+{
+    if (vectorData == NULL || index >= size)
+        return 0.0f;
+    return vectorData[index];
 }
