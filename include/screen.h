fe04cbea835495fc06e71b13da10a38505307b63
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-05 19:35:26 +0000

    draw triangle and min/max

--- include/screen.h
+++ include/screen.h
@@ -3,9 +3,10 @@
 
 #include <stddef.h>
 #include "types.h"
+#include "vector.h"
 
-#define WIDTH 20
-#define HEIGHT 10
+#define WIDTH 500
+#define HEIGHT 500
 #define BUFFER_SIZE (WIDTH * HEIGHT)
 #define X_OFFSET (WIDTH / 2)
 #define Y_OFFSET (HEIGHT / 2)
@@ -23,5 +24,6 @@ int getIndex(int x, int y);
 const struct Color get(const struct ScreenBuffer *sb, int x, int y);
 void set(struct ScreenBuffer *sb, int x, int y, struct Color color);
 const char *display(const struct ScreenBuffer *sb);
-
+void drawTriangle(struct ScreenBuffer *sb, struct Vector2 a, struct Vector2 b, struct Vector2 c, struct Color color);
+int calculateBarycentricCoordinates(struct Vector2 p, struct Vector2 a, struct Vector2 b, struct Vector2 c);
 #endif
