ef10594e9701dec9fdd43e473645bd7e02d5f200
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-06 17:24:41 +0200

    change utils file

--- include/utils.h
+++ include/utils.h
@@ -1,14 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-float max(float a, float b)
-{
-    return a > b ? a : b;
-}
-
-float min(float a, float b)
-{
-    return a < b ? a : b;
-}
+float max(float a, float b);
+float min(float a, float b);
 
 #endif
---
+++ src/utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+float max(float a, float b)
+{
+    return a > b ? a : b;
+}
+
+float min(float a, float b)
+{
+    return a < b ? a : b;
+}
