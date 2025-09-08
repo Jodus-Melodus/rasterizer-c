123e2279489897fd986c83689c2eeacc4ed9aae0
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-08 21:40:35 +0200

    update utils

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
-#include <stdlib.h>
 
-char *appendString(char *a, const char *b);
 unsigned char randColor();
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,33 +1,4 @@
-#include <string.h>
 #include "utils.h"
-
-char *appendString(char *a, const char *b)
-{
-    if (!b)
-        return a; // nothing to append
-
-    if (!a)
-    { // handle NULL base string
-        a = malloc(strlen(b) + 1);
-        if (!a)
-            return NULL;
-        strcpy(a, b);
-        return a;
-    }
-
-    size_t len_a = strlen(a);
-    size_t len_b = strlen(b);
-
-    char *tmp = realloc(a, len_a + len_b + 1);
-    if (!tmp)
-    {
-        free(a);
-        return NULL;
-    }
-
-    memcpy(tmp + len_a, b, len_b + 1); // safe append including '\0'
-    return tmp;
-}
 
 unsigned char randColor()
 {
